d8b313c1713883fad213cbf9d2741319e5ff2e55
Taggs33 <taggart3317@gmail.com>
2015-10-19 23:37:56 -0400

    created add/remove option

--- js/shop.js
+++ js/shop.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
 
+	$('.add-item-button').click(function() {
+		var item = $('.add-item-text').val();
+		$('.add-item-text').val('');
+
+		$('.list').append('<li class="item">' + item + '<i class="fa fa-times"></i></li>');
+	});
 });
+
+// <li class="item">Bread<i class="fa fa-times"></i></li>
